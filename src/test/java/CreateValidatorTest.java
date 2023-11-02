3bfa6e721bf080ba80710560368567718370dba5
bbqw-ng <brwng902@gmail.com>
2023-11-02 19:12:32 -0400

    Created test and setup -added a test case to test for insensitivity -added a beforeEach, setup bank and CreateValidator

--- src/test/java/CreateValidatorTest.java
+++ src/test/java/CreateValidatorTest.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CreateValidatorTest {
+    CreateValidator validator;
+
+    @BeforeEach
+    public void Setup(){
+        Bank bank = new Bank();
+        validator = new CreateValidator(bank);
+    }
+
+    @Test
+    public void case_insensitive_create_command(){
+        boolean actual = CreateValidator.validate("creAte chEcKinG 10002000 7");
+        assertTrue(actual);
+    }
 }
