720150a8a4dfb0fd902c151cf075400a094eb6c0
bbqw-ng <brwng902@gmail.com>
2023-11-03 12:51:30 -0400

    Added Additional Test corresponding to the class methods -Added check_valid_id, check_valid_apr, can_split_string, can_turn_command_lower_case

--- src/test/java/CreateValidatorTest.java
+++ src/test/java/CreateValidatorTest.java
@@ -1,20 +1,54 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CreateValidatorTest {
-    CreateValidator validator;
+
+    public static final String MOCK_TEST_CASE = "CREATE CHECKING 10002000 7";
+    CreateValidator createValidator;
 
     @BeforeEach
     public void Setup(){
         Bank bank = new Bank();
-        validator = new CreateValidator(bank);
+        createValidator = new CreateValidator(bank);
     }
 
     @Test
+    public void can_turn_command_lower_case(){
+        String actual = createValidator.turnLowerCase(MOCK_TEST_CASE);
+        assertEquals("create checking 10002000 7", actual);
+    }
+
+    @Test
+    public void can_split_string(){
+        String convert = createValidator.turnLowerCase(MOCK_TEST_CASE);
+        String[] actual = createValidator.stringParser(convert);
+
+        assertEquals("checking", actual[1]);
+    }
+
+    @Test
+    public void check_valid_id(){
+        String[] parsedList = createValidator.stringParser(createValidator.turnLowerCase(MOCK_TEST_CASE));
+        boolean actual = createValidator.checkValidId(parsedList);
+
+        assertTrue(actual);
+    }
+
+    @Test
+    public void check_valid_apr(){
+        String[] parsedList = createValidator.stringParser(createValidator.turnLowerCase(MOCK_TEST_CASE));
+        boolean actual = createValidator.checkValidApr(parsedList);
+
+        assertTrue(actual);
+    }
+
+
+    @Test
     public void case_insensitive_create_command(){
-        boolean actual = CreateValidator.validate("creAte chEcKinG 10002000 7");
+        boolean actual = createValidator.validate("creAte chEcKinG 10002000 7");
         assertTrue(actual);
     }
 }
