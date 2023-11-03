b527e6a272360cd860d49f2db61af5a8aa04a739
bbqw-ng <brwng902@gmail.com>
2023-11-03 11:02:55 -0400

    Added a couple methods -created a constructor for the main validator. -created a validate method to validate.

--- src/main/java/CreateValidator.java
+++ src/main/java/CreateValidator.java
@@ -1,2 +1,24 @@
 public class CreateValidator {
+    private Bank bank;
+
+    public CreateValidator(Bank bank){
+        this.bank = bank;
+
+    }
+
+    public static boolean validate(String command) {
+        return ;
+        //to test case insensitivity, you wanna create another helper function that turns the string into lower case first. :))
+        //so basically to test each category (class, id, apr, balance (for cd only)
+        //remember to create breaks and things
+        //you want to create helper functions to check the ids and see if it is unique and 8 digit.
+        // you want to check if apr between 0 or 10.
+        // you want to check the balance for starting between 1000 and 10000.
+        // check if class is valid ( a child of BankAccount parent class)
+        //make a switch to test if it is either "checking" "savings" or "cd"
+        // if class = 999io
+        //switch will test if 999io == "checking" , final case = break false.
+        //if statement that tests if it is cd, if it is then it goes through the conditional
+    }
+
 }
