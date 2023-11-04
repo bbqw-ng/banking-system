0bbc83c5be56cc37b6ee150c82dc7d9343a62563
bbqw-ng <brwng902@gmail.com>
2023-11-04 12:50:53 -0400

    Added and changed some methods and test in respective files -added try, catch checks to checkValidApr, checkValidId (createValidator)
    -added a new method "checkCreate" to check for "create" in parsed command (createValidator)
    
    -added a new test case "check_create_command" that checks for exactly "create" in first index of passed array.(createValidatorTest)

--- src/main/java/CreateValidator.java
+++ src/main/java/CreateValidator.java
@@ -1,54 +1,56 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
 public class CreateValidator {
-    private Bank bank;
+	private Bank bank;
 
-    public CreateValidator(Bank bank){
-        this.bank = bank;
+	public CreateValidator(Bank bank) {
+		this.bank = bank;
 
-    }
+	}
 
-    public boolean validate(String command) {
-        return true;
-        //to test case insensitivity, you wanna create another helper function that turns the string into lower case first. :))
-        //so basically to test each category (class, id, apr, balance (for cd only)
-        //remember to create breaks and things
-        //you want to create helper functions to check the ids and see if it is unique and 8 digit.
-        // you want to check if apr between 0 or 10.
-        // you want to check the balance for starting between 1000 and 10000.
-        // check if class is valid ( a child of BankAccount parent class)
-        //make a switch to test if it is either "checking" "savings" or "cd"
-        // if class = 999io
-        //switch will test if 999io == "checking" , final case = break false.
-        //if statement that tests if it is cd, if it is then it goes through the conditional
-    }
+	public boolean validate(String command) {
+		return true;
+		// to test case insensitivity, you wanna create another helper function that
+		// turns the string into lower case first. :))
+		// so basically to test each category (class, id, apr, balance (for cd only)
+		// remember to create breaks and things
+		// you want to create helper functions to check the ids and see if it is unique
+		// and 8 digit.
+		// you want to check if apr between 0 or 10.
+		// you want to check the balance for starting between 1000 and 10000.
+		// check if class is valid ( a child of BankAccount parent class)
+		// make a switch to test if it is either "checking" "savings" or "cd"
+		// if class = 999io
+		// switch will test if 999io == "checking" , final case = break false.
+		// if statement that tests if it is cd, if it is then it goes through the
+		// conditional
+	}
 
-    public String turnLowerCase(String command){
-        String lowerCased = command.toLowerCase();
-        return lowerCased;
-    }
+	public String turnLowerCase(String command) {
+		return command.toLowerCase();
+	}
 
-    public String[] stringParser(String command){
-        String[] arrayOfString = command.split(" ");
-        return arrayOfString;
-    }
+	public String[] stringParser(String command) {
+		return command.split(" ");
+	}
 
-    public boolean checkValidId(String[] array) {
-        if( array[2].length() == 8){
-            //insert a try catch for the parseInt to turn string to int if possible.
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public boolean checkValidId(String[] array) {
+		try {
+			int strToInt = Integer.parseInt(array[2]);
+			return (array[2].length() == 8);
+		} catch (Exception exception) {
+			return false;
+		}
+	}
 
-    public boolean checkValidApr(String[] array) {
-        if ( array[3].length() == 2 ) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public boolean checkValidApr(String[] array) {
+		try {
+			int strToInt = Integer.parseInt(array[3]);
+			return (strToInt >= 0 && strToInt <= 10);
+		} catch (Exception exception) {
+			return false;
+		}
+	}
+
+	public boolean checkCreate(String[] array) {
+		return (array[0].equals("create"));
+	}
 }
--- src/test/java/CreateValidatorTest.java
+++ src/test/java/CreateValidatorTest.java
@@ -1,54 +1,60 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CreateValidatorTest {
 
-    public static final String MOCK_TEST_CASE = "CREATE CHECKING 10002000 7";
-    CreateValidator createValidator;
+	public static final String MOCK_TEST_CASE = "CREATE CHECKING 10002000 7";
+	CreateValidator createValidator;
 
-    @BeforeEach
-    public void Setup(){
-        Bank bank = new Bank();
-        createValidator = new CreateValidator(bank);
-    }
+	@BeforeEach
+	public void Setup() {
+		Bank bank = new Bank();
+		createValidator = new CreateValidator(bank);
+	}
 
-    @Test
-    public void can_turn_command_lower_case(){
-        String actual = createValidator.turnLowerCase(MOCK_TEST_CASE);
-        assertEquals("create checking 10002000 7", actual);
-    }
+	@Test
+	public void can_turn_command_lower_case() {
+		String actual = createValidator.turnLowerCase(MOCK_TEST_CASE);
+		assertEquals("create checking 10002000 7", actual);
+	}
 
-    @Test
-    public void can_split_string(){
-        String convert = createValidator.turnLowerCase(MOCK_TEST_CASE);
-        String[] actual = createValidator.stringParser(convert);
+	@Test
+	public void can_split_string() {
+		String[] actual = createValidator.stringParser(createValidator.turnLowerCase(MOCK_TEST_CASE));
 
-        assertEquals("checking", actual[1]);
-    }
+		assertEquals("checking", actual[1]);
+	}
 
-    @Test
-    public void check_valid_id(){
-        String[] parsedList = createValidator.stringParser(createValidator.turnLowerCase(MOCK_TEST_CASE));
-        boolean actual = createValidator.checkValidId(parsedList);
+	@Test
+	public void check_valid_id() {
+		String[] parsedList = createValidator.stringParser(createValidator.turnLowerCase(MOCK_TEST_CASE));
+		boolean actual = createValidator.checkValidId(parsedList);
 
-        assertTrue(actual);
-    }
+		assertTrue(actual);
+	}
 
-    @Test
-    public void check_valid_apr(){
-        String[] parsedList = createValidator.stringParser(createValidator.turnLowerCase(MOCK_TEST_CASE));
-        boolean actual = createValidator.checkValidApr(parsedList);
+	@Test
+	public void check_valid_apr() {
+		String[] parsedList = createValidator.stringParser(createValidator.turnLowerCase(MOCK_TEST_CASE));
+		boolean actual = createValidator.checkValidApr(parsedList);
 
-        assertTrue(actual);
-    }
+		assertTrue(actual);
+	}
 
+	@Test
+	public void check_create_command() {
+		String[] parsedList = createValidator.stringParser(createValidator.turnLowerCase(MOCK_TEST_CASE));
+		boolean actual = createValidator.checkCreate(parsedList);
+		assertTrue(actual);
+	}
 
-    @Test
-    public void case_insensitive_create_command(){
-        boolean actual = createValidator.validate("creAte chEcKinG 10002000 7");
-        assertTrue(actual);
-    }
+	@Test
+	public void case_insensitive_create_command() {
+		boolean actual = createValidator.validate("creAte chEcKinG 10002000 7");
+
+		assertTrue(actual);
+	}
 }
