d447f80552c03a241d1ddc44bfd5709bc63d4d5b
bbqw-ng <brwng902@gmail.com>
2023-11-05 00:24:21 -0400

    Added new methods and refined old methods.
    -finished the validate method (included all the checks)
    
    -added balanceAndAccountChecker, checkAccountTypeFromBank, amountChecker, CheckIdInBank, CheckValidId, checkDeposit

--- src/main/java/DepositValidator.java
+++ src/main/java/DepositValidator.java
@@ -1,2 +1,94 @@
 public class DepositValidator {
+	private Bank bank;
+
+	public DepositValidator(Bank bank) {
+		this.bank = bank;
+	}
+
+	public boolean validate(String command) {
+		String lowerCaseCommand = turnLowerCase(command);
+		String[] parsedString = stringParser(lowerCaseCommand);
+
+		if (checkDeposit(parsedString)) {
+			if (checkValidId(parsedString)) {
+				if (amountChecker(parsedString)) {
+					if (checkIdInBank(parsedString)) {
+						return (balanceAndAccountChecker(checkAccountTypeFromBank(parsedString), parsedString));
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	public String turnLowerCase(String command) {
+		return command.toLowerCase();
+	}
+
+	public String[] stringParser(String command) {
+		return command.split(" ");
+	}
+
+	public boolean checkDeposit(String[] string) {
+		return (string[0].equals("deposit"));
+	}
+
+	public boolean checkValidId(String[] string) {
+		try {
+			int strToInt = Integer.parseInt(string[1]);
+			return (string[1].length() == 8 && strToInt > 0);
+		} catch (Exception exception) {
+			return false;
+		}
+	}
+
+	public boolean checkIdInBank(String[] string) {
+		try {
+			if (bank.retrieveAccountById(string[1]).getAccountId() == null) {
+				return false;
+			}
+			return true;
+		} catch (Exception exception) {
+			return true;
+		}
+	}
+
+	public boolean amountChecker(String[] string) {
+		try {
+			double convertAmount = Double.parseDouble(string[2]);
+			if (convertAmount >= 0) {
+				return true;
+			}
+			return false;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public String checkAccountTypeFromBank(String[] string) {
+		switch (bank.retrieveAccountById(string[1]).getAccountType()) {
+		case "savings":
+			return "savings";
+		case "checking":
+			return "checking";
+		case "cd":
+			return "cd";
+		}
+		return "none";
+	}
+
+	public boolean balanceAndAccountChecker(String string, String[] array) {
+		Double convertAmount = Double.parseDouble(array[2]);
+		if (string.equals("savings")) {
+			return (convertAmount >= 0 && convertAmount <= 2500);
+		}
+		if (string.equals("checking")) {
+			return (convertAmount >= 0 && convertAmount <= 1000);
+		}
+		if (string.equals("cd")) {
+			return false;
+		}
+		return false;
+	}
+
 }
