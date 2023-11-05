1a2121ba10e73bac8ffd141a3d55c02b777bd27b
bbqw-ng <brwng902@gmail.com>
2023-11-05 00:24:54 -0400

    Added new tests
    -added savings deposit test (valid)
    
    -added checking deposit test (valid)
    
    -added standalone deposit test (invalid)

--- src/test/java/DepositValidatorTest.java
+++ src/test/java/DepositValidatorTest.java
@@ -1,2 +1,46 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class DepositValidatorTest {
+	Bank bank;
+	DepositValidator depositValidator;
+	Checking checking;
+	Savings savings;
+	CD cd;
+
+	@BeforeEach
+	public void setUp() {
+		bank = new Bank();
+		depositValidator = new DepositValidator(bank);
+		checking = new Checking("10001000", 10);
+		savings = new Savings("20002000", 10);
+		cd = new CD("30003000", 10, 2000);
+		bank.addAccount(checking.getAccountId(), checking);
+		bank.addAccount(savings.getAccountId(), savings);
+		bank.addAccount(cd.getAccountId(), cd);
+	}
+
+	@Test
+	public void just_deposit_is_invalid() {
+		boolean actual = depositValidator.validate("deposit");
+
+		assertFalse(actual);
+	}
+
+	@Test
+	public void money_can_be_deposited_into_checking_account_is_valid() {
+		boolean actual = depositValidator.validate("Deposit 10001000 800");
+
+		assertTrue(actual);
+	}
+
+	@Test
+	public void money_can_be_deposited_into_savings_account_is_valid() {
+		boolean actual = depositValidator.validate("Deposit 20002000 800");
+
+		assertTrue(actual);
+	}
 }
