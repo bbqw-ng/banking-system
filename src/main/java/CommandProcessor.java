50ee52991af57c08db71e049da775f26522d57d3
Brian Wang <brwng902@gmail.com>
2023-11-11 18:03:31 -0500

    Removed Private methods and instead placed them into their respective child classes. Changed process method to accompany child classes as well.
    -removed the 4 private methods for cd savings checking and deposit and moved them into respective files
    
    -modified the process method to instead call the child classes instead of the private methods.

--- src/main/java/CommandProcessor.java
+++ src/main/java/CommandProcessor.java
@@ -1,8 +1,7 @@
 public class CommandProcessor {
 	private Bank bank;
-	private Checking checking;
-	private Savings savings;
-	private CD cd;
+	private CreateProcessor createProcessor;
+	private DepositProcessor depositProcessor;
 
 	public CommandProcessor(Bank bank) {
 		this.bank = bank;
@@ -11,19 +10,11 @@ public class CommandProcessor {
 	public void process(String command) {
 		String[] parsedString = parseString(command);
 		if (parsedString[0].equals("create")) {
-			switch (parsedString[1]) {
-			case ("checking"):
-				processChecking(parsedString);
-				break;
-			case ("savings"):
-				processSavings(parsedString);
-				break;
-			case ("cd"):
-				processCD(parsedString);
-				break;
-			}
+			createProcessor = new CreateProcessor(bank);
+			createProcessor.process(parsedString);
 		} else if (parsedString[0].equals("deposit")) {
-			processDeposit(parsedString);
+			depositProcessor = new DepositProcessor(bank);
+			depositProcessor.process(parsedString);
 		}
 	}
 
@@ -31,31 +22,4 @@ public class CommandProcessor {
 		return command.toLowerCase().split(" ");
 	}
 
-	private void processChecking(String[] parsed) {
-		String id = parsed[2];
-		double apr = Double.parseDouble(parsed[3]);
-		checking = new Checking(id, apr);
-		bank.addAccount(checking.getAccountId(), checking);
-	}
-
-	private void processCD(String[] parsed) {
-		String id = parsed[2];
-		double apr = Double.parseDouble(parsed[3]);
-		double balance = Double.parseDouble(parsed[4]);
-		cd = new CD(id, apr, balance);
-		bank.addAccount(cd.getAccountId(), cd);
-	}
-
-	private void processSavings(String[] parsed) {
-		String id = parsed[2];
-		double apr = Double.parseDouble(parsed[3]);
-		savings = new Savings(id, apr);
-		bank.addAccount(savings.getAccountId(), savings);
-	}
-
-	private void processDeposit(String[] parsed) {
-		String id = parsed[1];
-		double amount = Double.parseDouble(parsed[2]);
-		bank.getAccountById(id).doDeposit(amount);
-	}
 }
