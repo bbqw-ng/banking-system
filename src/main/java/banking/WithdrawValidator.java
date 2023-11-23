b0cc1364c9709b5dbf66a9624d924cfb09faab5a
Brian Wang <brwng902@gmail.com>
2023-11-23 16:43:13 -0500

    added constructors/methods/validator for WithdrawValidator
    -added validate method to validate each part of the command
    
    -added methods to assist validate to check each part of the command: balanceAndAccountChecker, amountChecker, checkValidId

--- src/main/java/banking/WithdrawValidator.java
+++ src/main/java/banking/WithdrawValidator.java
@@ -1,4 +1,61 @@
 package banking;
 
-public class WithdrawValidator {
+public class WithdrawValidator extends CommandValidator {
+
+	public WithdrawValidator(Bank bank) {
+		super(bank);
+	}
+
+	public boolean validate(String[] parsedString) {
+		if (checkWithdraw(parsedString)) {
+			if (checkValidId(parsedString)) {
+				if (amountChecker(parsedString)) {
+					if (super.checkDepositAndWithdrawIdInBank(parsedString)) {
+						return (balanceAndAccountChecker(checkAccountTypeFromBank(parsedString), parsedString));
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	// note We just finished the validate function now we have to establish some
+	// tests to check and validate each portion
+	// we did not implement the pass time function into this so as of right now we
+	// did not do anything about the withdraw limit per months
+	@Override
+	public boolean checkValidId(String[] parsedString) {
+		try {
+			int strToInt = Integer.parseInt(parsedString[1]);
+			return (parsedString[1].length() == 8 && strToInt >= 0);
+		} catch (Exception exception) {
+			return false;
+		}
+	}
+
+	public boolean amountChecker(String[] string) {
+		try {
+			double convertAmount = Double.parseDouble(string[2]);
+			if (convertAmount >= 0) {
+				return true;
+			}
+			return false;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public boolean balanceAndAccountChecker(String accType, String[] parsedString) {
+		Double convertAmount = Double.parseDouble(parsedString[2]);
+		if (accType.equals("savings")) {
+			return (convertAmount >= 0 && convertAmount <= 1000);
+		}
+		if (accType.equals("checking")) {
+			return (convertAmount >= 0 && convertAmount <= 400);
+		}
+		if (accType.equals("cd")) {
+			return false;
+		}
+		return false;
+	}
 }
