2577fd58c8475d92f770cf69f3da3842bca96186
Brian Wang <brwng902@gmail.com>
2023-12-02 12:59:02 -0500

    Made files test their respective processors and not the parent processor.
    -changed areas where commandProcessor was called into their respective processors instead of the main processor.

--- src/test/java/banking/DepositProcessorTest.java
+++ src/test/java/banking/DepositProcessorTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Test;
 
 public class DepositProcessorTest {
 	Bank bank;
-	CommandProcessor commandProcessor;
+	DepositProcessor depositProcessor;
 	Checking checking;
 	Savings savings;
 
 	@BeforeEach
 	public void setUp() {
 		bank = new Bank();
-		commandProcessor = new CommandProcessor(bank);
+		depositProcessor = new DepositProcessor(bank);
 		checking = new Checking("11112222", 9);
 		savings = new Savings("22223333", 9);
 		bank.addAccount(checking.getAccountId(), checking);
@@ -23,19 +23,19 @@ public class DepositProcessorTest {
 
 	@Test
 	public void can_process_a_deposit_command() {
-		commandProcessor.process("deposit 22223333 1");
+		depositProcessor.process("deposit 22223333 1");
 		assertEquals(bank.getAccountById("22223333").getBalance(), 1);
 	}
 
 	@Test
 	public void can_deposit_into_checking_account() {
-		commandProcessor.process("deposit 11112222 500");
+		depositProcessor.process("deposit 11112222 500");
 		assertEquals(bank.getAccountById("11112222").getBalance(), 500);
 	}
 
 	@Test
 	public void can_deposit_into_savings_account() {
-		commandProcessor.process("deposit 22223333 1000");
+		depositProcessor.process("deposit 22223333 1000");
 		assertEquals(bank.getAccountById("22223333").getBalance(), 1000);
 	}
 
@@ -43,7 +43,7 @@ public class DepositProcessorTest {
 	public void can_deposit_into_checking_account_with_existing_balance() {
 		bank.deposit("11112222", 1000);
 
-		commandProcessor.process("deposit 11112222 500");
+		depositProcessor.process("deposit 11112222 500");
 		assertEquals(bank.getAccountById("11112222").getBalance(), 1500);
 	}
 
@@ -51,7 +51,7 @@ public class DepositProcessorTest {
 	public void can_deposit_into_savings_account_with_existing_balance() {
 		bank.deposit("22223333", 2000);
 
-		commandProcessor.process("deposit 22223333 1000");
+		depositProcessor.process("deposit 22223333 1000");
 		assertEquals(bank.getAccountById("22223333").getBalance(), 3000);
 	}
 }
