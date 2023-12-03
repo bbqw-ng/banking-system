55f0a0339828b675e27db8ce921c529b6d660c0b
Brian Wang <brwng902@gmail.com>
2023-12-03 18:18:53 -0500

    removed some comments

--- src/main/java/banking/Bank.java
+++ src/main/java/banking/Bank.java
@@ -73,19 +73,12 @@ public class Bank {
 						if ("savings".equals(account.getAccountType())) {
 							account.canWithdraw(true);
 						}
-
 						doNormalAprCalc(account);
 					} else if ("cd".equals(account.getAccountType())) {
 
 						if (months >= 12) {
 							account.canWithdraw(true);
 						}
-
-						// what if i withdraw once the amount and then the balance is 0 and then I
-						// withdraw again? Do I make it so
-						// I cannot withdraw (aka flip the switch to false) or keep it true and you can
-						// withdraw as many 0's as you want until another month passes and the account
-						// closes
 						doCdAprCalc(account);
 					}
 				}
