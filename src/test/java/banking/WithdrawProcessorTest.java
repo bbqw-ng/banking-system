518ad92db37d052c3cd761ebe31335303587457e
Brian Wang <brwng902@gmail.com>
2023-12-04 23:00:53 -0500

    added multiple tests to further test functionality

--- src/test/java/banking/WithdrawProcessorTest.java
+++ src/test/java/banking/WithdrawProcessorTest.java
@@ -102,4 +102,133 @@ public class WithdrawProcessorTest {
 		withdrawProcessor.process("withdraw 30003000 5000");
 		assertEquals(0, bank.getAccountById("30003000").getBalance());
 	}
+
+	@Test
+	public void can_withdraw_from_checking_account_twice() {
+		bank.deposit("10001000", 1000);
+		withdrawProcessor.process("withdraw 10001000 100");
+		withdrawProcessor.process("withdraw 10001000 100");
+		assertEquals(800, bank.getAccountById("10001000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_from_checking_account_twice_in_the_same_month() {
+		bank.deposit("10001000", 1000);
+		bank.pass(1);
+		withdrawProcessor.process("withdraw 10001000 100");
+		withdrawProcessor.process("withdraw 10001000 100");
+		assertEquals(aprCalc(1000, 5) - 200, bank.getAccountById("10001000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_from_savings_account_twice_in_two_different_months() {
+		bank.deposit("20002000", 1000);
+		withdrawProcessor.process("withdraw 20002000 100");
+		bank.pass(1);
+		withdrawProcessor.process("withdraw 20002000 100");
+		assertEquals(aprCalc(900, 5) - 100, bank.getAccountById("20002000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_1000_from_savings_account() {
+		bank.deposit("20002000", 1000);
+		withdrawProcessor.process("withdraw 20002000 1000");
+		assertEquals(0, bank.getAccountById("20002000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_0_from_savings_account() {
+		bank.deposit("20002000", 1000);
+		withdrawProcessor.process("withdraw 20002000 0");
+		assertEquals(1000, bank.getAccountById("20002000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_400_from_checking_account() {
+		bank.deposit("10001000", 400);
+		withdrawProcessor.process("withdraw 10001000 400");
+		assertEquals(0, bank.getAccountById("10001000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_0_from_checking_account() {
+		bank.deposit("10001000", 400);
+		withdrawProcessor.process("withdraw 10001000 0");
+		assertEquals(400, bank.getAccountById("10001000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_over_balance_from_cd_when_13_months_pass() {
+		bank.pass(13);
+		withdrawProcessor.process("withdraw 30003000 1000000");
+		assertEquals(0, bank.getAccountById("30003000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_over_balance_from_cd_when_60_months_pass() {
+		bank.pass(60);
+		withdrawProcessor.process("withdraw 30003000 10000000");
+		assertEquals(0, bank.getAccountById("30003000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_over_balance_from_savings_when_60_months_pass() {
+		bank.deposit("20002000", 1000);
+		bank.pass(60);
+		withdrawProcessor.process("withdraw 20002000 1000000");
+		assertEquals(0, bank.getAccountById("20002000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_over_balance_from_checking_when_60_months_pass() {
+		bank.deposit("10001000", 2000);
+		bank.pass(60);
+		withdrawProcessor.process("withdraw 10001000 20123123");
+		assertEquals(0, bank.getAccountById("10001000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_checking_and_savings_in_the_same_month() {
+		bank.deposit("10001000", 2000);
+		bank.deposit("20002000", 1000);
+		bank.pass(1);
+		withdrawProcessor.process("withdraw 10001000 20123123");
+		withdrawProcessor.process("withdraw 20002000 1000000");
+		assertEquals(0, bank.getAccountById("10001000").getBalance());
+		assertEquals(0, bank.getAccountById("20002000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_checking_and_savings_and_cd_after_12_months() {
+		bank.deposit("10001000", 2000);
+		bank.deposit("20002000", 1000);
+		bank.pass(12);
+		withdrawProcessor.process("withdraw 10001000 20123123");
+		withdrawProcessor.process("withdraw 20002000 1000000");
+		withdrawProcessor.process("withdraw 30003000 1912938");
+		assertEquals(0, bank.getAccountById("10001000").getBalance());
+		assertEquals(0, bank.getAccountById("20002000").getBalance());
+		assertEquals(0, bank.getAccountById("30003000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_checking_and_cd_after_12_months() {
+		bank.deposit("10001000", 2000);
+		bank.pass(12);
+		withdrawProcessor.process("withdraw 10001000 20123123");
+		withdrawProcessor.process("withdraw 30003000 1912938");
+		assertEquals(0, bank.getAccountById("10001000").getBalance());
+		assertEquals(0, bank.getAccountById("30003000").getBalance());
+	}
+
+	@Test
+	public void can_withdraw_checking_and_savings_after_12_months() {
+		bank.deposit("10001000", 2000);
+		bank.deposit("20002000", 1000);
+		bank.pass(12);
+		withdrawProcessor.process("withdraw 10001000 20123123");
+		withdrawProcessor.process("withdraw 20002000 1000000");
+		assertEquals(0, bank.getAccountById("10001000").getBalance());
+		assertEquals(0, bank.getAccountById("20002000").getBalance());
+	}
 }
