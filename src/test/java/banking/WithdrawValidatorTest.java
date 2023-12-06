7b0087e2f2ae812e2884af8bff951425a17c2737
Brian Wang <brwng902@gmail.com>
2023-12-06 01:14:31 -0500

    added comment

--- src/test/java/banking/WithdrawValidatorTest.java
+++ src/test/java/banking/WithdrawValidatorTest.java
@@ -307,4 +307,9 @@ public class WithdrawValidatorTest {
 		assertFalse(check);
 	}
 
+	// need to validate all the pass time rules.
+	// checking infinite withdraws
+	// savings 1 per month
+	// cd cannot transfer at all.
+
 }
