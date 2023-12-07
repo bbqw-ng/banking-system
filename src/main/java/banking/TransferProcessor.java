0ef1863d5e207ae014c0b0a9e08ef3b09c5baef9
Brian Wang <brwng902@gmail.com>
2023-12-06 19:36:37 -0500

    added main process function and constructor
    -added process function with checks for deposit / withdraw inbuilt

---
+++ src/main/java/banking/TransferProcessor.java
@@ -0,0 +1,26 @@
+package banking;
+
+public class TransferProcessor extends CommandProcessor {
+
+	public TransferProcessor(Bank bank) {
+		super(bank);
+	}
+
+	public void process(String[] parsedString) {
+		double amount = Double.parseDouble(parsedString[3]);
+		String senderId = parsedString[1];
+		String receiverId = parsedString[2];
+		if (bank.getAccountById(senderId).getAllowWithdraw() == true) {
+			if (amount > bank.getAccountById(senderId).getBalance()) {
+				bank.deposit(receiverId, bank.getAccountById(senderId).getBalance());
+			} else {
+				bank.deposit(receiverId, amount);
+			}
+			bank.withdraw(parsedString[1], amount);
+
+			if (bank.getAccountById(senderId).getAccountType().equals("savings")) {
+				bank.getAccountById(senderId).canWithdraw(false);
+			}
+		}
+	}
+}
