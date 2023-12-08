6c3312c1823f4a06f5e718efb3e8337f45c9c2d9
bbqw-ng <brwng902@gmail.com>
2023-12-07 20:13:22 -0500

    added a var that changes command to lowercase, in each transaction command - added the command to respective accounts.

--- src/main/java/banking/CommandProcessor.java
+++ src/main/java/banking/CommandProcessor.java
@@ -13,21 +13,26 @@ public class CommandProcessor {
 	}
 
 	public void process(String command) {
-		String[] parsedString = parseString(command);
+		String lowerCased = command.toLowerCase();
+		String[] parsedString = parseString(lowerCased);
 		if (parsedString[0].equals("create")) {
 			createProcessor = new CreateProcessor(bank);
 			createProcessor.process(parsedString);
 		} else if (parsedString[0].equals("deposit")) {
 			depositProcessor = new DepositProcessor(bank);
+			bank.getAccountById(parsedString[1]).addAssociatedCommand(command);
 			depositProcessor.process(parsedString);
 		} else if (parsedString[0].equals("pass")) {
 			passTimeProcessor = new PassTimeProcessor(bank);
 			passTimeProcessor.process(parsedString);
 		} else if (parsedString[0].equals("withdraw")) {
 			withdrawProcessor = new WithdrawProcessor(bank);
+			bank.getAccountById(parsedString[1]).addAssociatedCommand(command);
 			withdrawProcessor.process(parsedString);
 		} else if (parsedString[0].equals("transfer")) {
 			transferProcessor = new TransferProcessor(bank);
+			bank.getAccountById(parsedString[1]).addAssociatedCommand(command);
+			bank.getAccountById(parsedString[2]).addAssociatedCommand(command);
 			transferProcessor.process(parsedString);
 		}
 	}
